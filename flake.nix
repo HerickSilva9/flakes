d6becff06707edfc9b6e995e9dbafb673c1f1a90
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2026-01-10 17:28:31 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Basic flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "project";
+        
+        packages = with pkgs; [
+          # git
+        ];
+
+        # shellHook = ''
+        # 
+        # '';
+      };
+    };
+}
