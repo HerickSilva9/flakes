cda517aefc19e0775f53471819e0b2ff7f963927
hericksilva9 <hericksilva9@outlook.com>
2026-02-07 23:36:00 -0300

    Refactor Python environment setup: enhance .envrc, update .gitignore, and modify flake.nix for improved virtual environment management

--- python/flake.nix
+++ python/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Nix-flake-based Python development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
@@ -12,14 +12,15 @@
       devShells.${system}.default = pkgs.mkShell rec {
         
         packages = with pkgs; [
-          python313
-          uv
+          uv  # Let uv manage the Python version
         ];
         
+        # Environment variables available in the shell
         env = {
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
-	      };
+          # LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
+        };
 
+        # Commands to run when entering the shell
         # shellHook = ''
         # 
         # '';
